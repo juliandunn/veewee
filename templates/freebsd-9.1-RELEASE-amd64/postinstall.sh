37f52cfc12720fc51c503cc08d003dabd3291df1
Julian C. Dunn <jdunn@aquezada.com>
2013-02-28 18:20:55 -0500

    Install packages using pkg_add, otherwise there isn't enough memory in MFSBSD to do this.

--- templates/freebsd-9.1-RELEASE-amd64/postinstall.sh
+++ templates/freebsd-9.1-RELEASE-amd64/postinstall.sh
@@ -2,51 +2,18 @@ date > /etc/vagrant_box_build_time
 
 ntpdate 0.pool.ntp.org
 
-# Get the latest portstree (needed for virtualbox to be on 4.x)
-portsnap fetch update
-portsnap extract
+export PACKAGESITE=ftp://ftp.freebsd.org/pub/FreeBSD/ports/amd64/packages-9-stable/Latest/
+for i in sudo bash rubygem-chef puppet wget; do pkg_add -r $i ; done
 
-#First install sudo
-cd /usr/ports/security/sudo
-make install -DBATCH
+# Install vagrant user
+pw useradd -n vagrant -c "Vagrant User" -m
+mkdir ~vagrant/.ssh
+chmod 700 ~vagrant/.ssh
+/usr/local/bin/wget --no-check-certificate 'https://raw.github.com/mitchellh/vagrant/master/keys/vagrant.pub' -O ~vagrant/.ssh/authorized_keys
+chown -R vagrant:vagrant /home/vagrant/.ssh/authorized_keys
 
-#We prefer bash to be there
-cd /usr/ports/shells/bash
-make install -DBATCH
-
-#Off to rubygems to get first ruby running
-cd /usr/ports/devel/ruby-gems
-make install -DBATCH
-
-#Gem chef - 10.24.0
-cd /usr/ports/sysutils/rubygem-chef
-make install -DBATCH
-
-#Gem puppet
-cd /usr/ports/sysutils/puppet
-make install -DBATCH
-
-#Get wget
-cd /usr/ports/ftp/wget
-make install -DBATCH
-
-#Installing vagrant keys
-mkdir /home/vagrant/.ssh
-chmod 700 /home/vagrant/.ssh
-cd /home/vagrant/.ssh
-/usr/local/bin/wget --no-check-certificate 'https://raw.github.com/mitchellh/vagrant/master/keys/vagrant.pub' -O authorized_keys
-chown -R vagrant /home/vagrant/.ssh
-
-# Cleaning portstree to save space
-# http://www.freebsd.org/doc/en_US.ISO8859-1/books/handbook/ports-using.html
-cd /usr/ports/ports-mgmt/portupgrade
-make install -DBATCH clean
-
-/usr/local/sbin/portsclean -C
-
-# As sharedfolders are not in defaults ports tree
-# We will use vagrant via NFS
-# Enable NFS
+# As FreeBSD's virtualbox guest additions do not support shared folders,
+# we will use vagrant via NFS
 echo 'rpcbind_enable="YES"' >> /etc/rc.conf
 echo 'nfs_server_enable="YES"' >> /etc/rc.conf
 echo 'mountd_flags="-r"' >> /etc/rc.conf
@@ -69,27 +36,7 @@ echo "4) Use nfs instead of shared folders"
 echo "		:nfs => true"
 echo "============================================================================="
 
-
-
-exit
-
-# The iso from virtualbox will only install windows/solaris or linux, no BSD
-# Research is on it's way to have 4.x in the main portstree
-# http://www.listware.net/201102/freebsd-ports/65201-call-for-testers-virtualbox-404.html
-# Virtualbox additions - http://wiki.freebsd.org/VirtualBox
-# Currently this will only work for 4.0.4
-#cd /tmp
-#wget http://home.bluelife.at/ports/virtualbox-cft-20110218.tar.gz
-#cd /usr/ports
-#tar -xzvf /tmp/virtualbox-cft-20110218.tar.gz
-
-# This requires libtool >= 2.4
-cd /usr/ports/devel/libtool
-make clean
-make install -DBATCH
-
-cd /usr/ports/emulators/virtualbox-ose-additions
-make install -DBATCH
+pkg_add -r virtualbox-ose-additions
 
 echo 'vboxguest_enable="YES"' >> /etc/rc.conf
 echo 'vboxservice_enable="YES"' >> /etc/rc.conf
